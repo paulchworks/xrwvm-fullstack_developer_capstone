1b8035e747812a2449dbf5e0162f44f41172e0b7
paulchworks <paulchworks@gmail.com>
2024-11-12 12:00:58 -0500

    added dealer views

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,9 +15,24 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-
-    # path for add a review view
+    path(route="get_dealers/", view=views.get_dealerships, name="get_dealers"),
+    path(
+        route="get_dealers/<str:state>",
+        view=views.get_dealerships,
+        name="get_dealers_by_state",
+        ),
+    path(
+        route="dealer/<int:dealer_id>",
+        view=views.get_dealer_details,
+        name="dealer_details",
+    ),
+    path(
+        route="reviews/dealer/<int:dealer_id>",
+        view=views.get_dealer_reviews,
+        name="dealer_details",
+    ),
+    path(route="add_review", view=views.add_review, name="add_review"),
+    path(route='get_inventory/<int:dealer_id>', view=views.get_inventory, name='get_inventory'),
 
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,6 +15,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 
 # Get an instance of a logger
@@ -80,30 +81,82 @@ def registration(request):
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-def get_dealerships(request):
-    count = CarMake.objects.filter().count()
-    print(count)
-    if(count == 0):
-        initiate()
-    car_models = CarModel.objects.select_related('car_make')
-    cars = []
-    for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
-    return JsonResponse({"CarModels":cars})
-    
+def get_dealerships(request, state="All"):
+    if state == "All":
+        endpoint = "fetchDealers"
+    else:
+        endpoint = "fetchDealers/" + state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 # def get_dealer_reviews(request,dealer_id):
-# ...
+def get_dealer_reviews(request, dealer_id):
+    # if dealer id has been provided
+    if dealer_id:
+        endpoint = "/fetchReviews/dealer/" + str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail["review"])
+            print(response)
+            review_detail["sentiment"] = response["sentiment"]
+        return JsonResponse({"status": 200, "reviews": reviews})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
 
 # Create a `get_dealer_details` view to render the dealer details
 # def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if dealer_id:
+        endpoint = "/fetchDealer/" + str(dealer_id)
+        dealership = get_request(endpoint)
+        return JsonResponse({"status": 200, "dealer": dealership})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
 
 # Create a `add_review` view to submit a review
 # def add_review(request):
-# ...
+def add_review(request):
+    if request.user.is_anonymous is False:
+        data = json.loads(request.body)
+        try:
+            # response = post_review(data)
+            return JsonResponse({"status": 200})
+        except Exception as err:
+            return JsonResponse(
+                {"status": 401, "message": ("Error in posting review: " + str(err))}
+            )
+    else:
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
+
+# Module import
+from .restapis import get_request, analyze_review_sentiments, post_review, searchcars_request
+
+# Get the inventory of a specific dealer
+def get_inventory(request, dealer_id):
+    data = request.GET
+    if (dealer_id):
+        if 'year' in data:
+            endpoint = "/carsbyyear/"+str(dealer_id)+"/"+data['year']
+        elif 'make' in data:
+            endpoint = "/carsbymake/"+str(dealer_id)+"/"+data['make']
+        elif 'model' in data:
+            endpoint = "/carsbymodel/"+str(dealer_id)+"/"+data['model']
+        elif 'mileage' in data:
+            endpoint = "/carsbymaxmileage/"+str(dealer_id)+"/"+data['mileage']
+        elif 'price' in data:
+            endpoint = "/carsbyprice/"+str(dealer_id)+"/"+data['price']
+        else:
+            endpoint = "/cars/"+str(dealer_id)
+ 
+        cars = searchcars_request(endpoint)
+        return JsonResponse({"status": 200, "cars": cars})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+    return JsonResponse({"status": 400, "message": "Bad Request"})
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
