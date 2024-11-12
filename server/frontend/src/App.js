72cbd09af222781fb76de1bfe87b45774de32ddc
paulchworks <paulchworks@gmail.com>
2024-11-12 12:35:56 -0500

    Added more Dealer views

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,7 +1,10 @@
-import LoginPanel from "./components/Login/Login"
-import Register from "./components/Register/Register";
+import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
-import Dealers from './components/Dealers/Dealers'
+import RegisterPanel from "./components/Register/Register";
+import Dealers from "./components/Dealers/Dealers";
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview";
+import SearchCars from "./components/Dealers/SearchCars";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register/>} />
       <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview />} />
+      <Route path="/searchcars/:id" element={<SearchCars />} />
     </Routes>
   );
 }
