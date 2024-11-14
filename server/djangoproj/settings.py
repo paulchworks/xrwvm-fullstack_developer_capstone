a77e39e721c0d208db62835796000b58b8bfaab7
PaulchWorks <107820572+paulchworks@users.noreply.github.com>
2024-11-14 12:33:35 +0800

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =
-['localhost',
+ALLOWED_HOSTS = ['localhost',
  'https://paulchowkums-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS =
 ['https://paulchowkums-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
