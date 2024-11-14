c55f2d7f794c7e7f7cb7060a5e839f7bc8d44bef
PaulchWorks <107820572+paulchworks@users.noreply.github.com>
2024-11-14 12:17:18 +0800

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -165,13 +165,15 @@ def get_inventory(request, dealer_id):
         return JsonResponse({"status": 400, "message": "Bad Request"})
     return JsonResponse({"status": 400, "message": "Bad Request"})
 
+
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
+    if (count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name, "CarMake":
+                     car_model.car_make.name})
     return JsonResponse({"CarModels":cars})
