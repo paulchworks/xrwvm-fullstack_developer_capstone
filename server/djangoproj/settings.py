b030e1b783436cb45e042ead59b7413a73da26b5
paulchworks <paulchworks@gmail.com>
2024-11-14 21:39:28 -0500

    added dockerfile and deployment config

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,8 +31,8 @@ HOST = 'https://paulchowkums-8000.theiadockernext-1-labs-prod-\
 theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ALLOWED_HOSTS = ['localhost', HOST]
 CSRF_TRUSTED_ORIGINS = [
-    'https://paulchowkums-8000.theiadockernext-1-labs-prod-\
-    theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://paulchowkums-8000.theiadockernext-0-labs-prod-\
+    theiak8s-4-tor01.proxy.cognitiveclass.ai/']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
